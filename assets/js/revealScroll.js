425258d8743b98c42989f95d060712e2b6e0c06b
ClementG60 <cle_guillaume@hotmail.fr>
2022-02-19 21:38:56 +0100

    modification competence

--- assets/js/revealScroll.js
+++ assets/js/revealScroll.js
@@ -15,10 +15,21 @@ const handleIntersect = function (entries, observer) {
 })
 }
 
+const handleIntersectSkills = function (entries, observerRight) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add("revealActiveSide")
+            observerSkills.unobserve(entry.target)
+        }
+})
+}
+
 const observer = new IntersectionObserver(handleIntersect, options)
 document.querySelectorAll(".revealUp").forEach(function (revealContainer) {
     observer.observe(revealContainer)
 })
 
-
-
+const observerSkills = new IntersectionObserver(handleIntersectSkills, options)
+document.querySelectorAll(".revealRight, .revealLeft").forEach(function (revealContainerSkills) {
+    observer.observe(revealContainerSkills)
+})
